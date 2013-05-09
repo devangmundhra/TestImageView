c2a93210437d0327e7a7298208c936a33dec05ec
Devang Mundhra <devangmundhra@gmail.com>
2013-05-09 11:19:55 -0700

    initial commit

--- TestImageView/ViewController.m
+++ TestImageView/ViewController.m
@@ -7,17 +7,31 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
 @end
 
 @implementation ViewController
+
+@synthesize imageView1 = _imageView1;
+@synthesize imageView2 = _imageView2;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.imageView1 setImageWithURL:[NSURL URLWithString:@"http://files.parse.com/ec0335d6-135b-418a-a5d0-3f697be04c70/3fba291b-f680-4be8-9680-180ff2be227e-file"] placeholderImage:nil];
+    [self.imageView2 setImageWithURL:[NSURL URLWithString:@"http://3.bp.blogspot.com/-AEuckzpxKs8/TZYrUXqHKBI/AAAAAAAAB6o/3Cbuw1h1pPI/s1600/Sailboats_at_sunset_art_wallpaper.jpg"] placeholderImage:nil];
+}
+
+- (void)viewDidUnload
+{
+    [self setImageView1:nil];
+    [self setImageView2:nil];
 }
 
 - (void)didReceiveMemoryWarning
